bfaadc967cad6fa34ea073d729a30a65002d3570
Kyle Thompson <rkthompson1414@gmail.com>
2025-03-11 11:54:53 -0700

    Workiing on identity

--- NlStudyTasks/Combinatorics.lean
+++ NlStudyTasks/Combinatorics.lean
@@ -17,14 +17,37 @@ def fac : Nat → Nat
 
 notation n "!" => fac n
 
--- def choose (n k : Nat) : Nat := (n)! / ((k)! * (n - k)!)
+def shiftRange (r : Finset ℕ) (s : ℕ) : Finset ℕ :=
+  r.map ⟨λ i => i + s, by simp[Function.Injective]⟩
 
--- range from 1 to 1000
-def y := Finset.range 1000
-#eval ∑ i ∈ y, i
+-- range from s to e inclusive
+@[simp]
+def range (s e : ℕ) : Finset ℕ :=
+  shiftRange (Finset.range (e - s + 1)) s
+
+theorem shift_sum (s : Finset ℕ) (shift : ℕ) (f : ℕ → ℕ) :
+  ∑ i ∈ range 0 n
+
+theorem sum_shift (n : ℕ) : ∑ i ∈ range 0 n, n.choose i = 1 + ∑ i ∈ range 1 n, n.choose i := by
+  induction n with
+  | zero => simp
+  | succ n' ih =>
+
+
+theorem powerset_card (n : ℕ) :
+  ∑ i ∈ range 0 n, Nat.choose n i = 2 ^ n := by
+  induction n with
+  | zero => simp
+  | succ n' ih =>
+    -- ∑_i=0^{n+1} (n+1 choose i) = 1 + ∑_i=1^{n+1} (n+1 choose i)
+    have h1: ∑ i ∈ range 0 (n' + 1), Nat.choose (n' + 1) i = 1 + ∑ i ∈ range 1 (n' + 1), Nat.choose (n' + 1) i := by
+      ring_nf
+
+
 
 theorem binom_theorem (a b n : Nat) :
   (a + b) ^ n = ∑ i ∈ Finset.range (n + 1), Nat.choose n i * (a ^ i) * (b ^ (n - i)):= by
   induction n with
   | zero => simp
-  | succ n ih => sorry
+  | succ n ih =>
+    calc ∑ i ∈ Finset.range (n + 1 + 1), (n + 1).choose i * a ^ i * b ^ (n + 1 - i) =
---
+++ NlStudyTasks/Fresh.lean
@@ -0,0 +1,24 @@
+
+import Mathlib.Tactic
+
+
+theorem sum_finset_union (s1 s2 : Finset ℕ) (f : ℕ → ℕ) :
+  ∑ i ∈ s1 ∪ s2, f i = ∑ i ∈ s1, f i + ∑ i ∈ s2, f i := by
+  apply Finset.sum_union
+
+
+theorem sum_choose_exp (n : ℕ) : ∑ i ∈ Finset.range (n + 1), n.choose i = 2^n := by
+  induction n with
+  | zero => simp
+  | succ n' ih =>
+    let newRange := Finset.range (n'+ 1 + 1) \ {0}
+    have h_union : Finset.range (n'+ 1 + 1) = newRange ∪ {0} := by simp [newRange]
+    rw [h_union]
+    have h_one_plus : ∑ i ∈ newRange ∪ {0}, (n'+ 1).choose i =
+        ∑ i ∈ newRange, (n'+ 1).choose i + ∑ i ∈ {0}, (n' + 1).choose i := by
+        have h_disjoint : Disjoint newRange {0} := by simp [newRange]
+        apply (Finset.sum_union h_disjoint)
+    rw [h_one_plus]
+
+    have h_newRange : newRange ∪ {0} = Finset.range (n' + 1 + 1) := by
+      simp [newRange]
